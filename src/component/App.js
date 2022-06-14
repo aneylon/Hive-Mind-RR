e0625722e5885a44366b59060714ac9771430702
aneylon <arlen.m.neylon@gmail.com>
2022-06-13 21:17:40 -0600

    Setup.

---
+++ src/component/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './Home'
+import About from './About'
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <About />
+    </div>
+  )
+}
+
+export default App
