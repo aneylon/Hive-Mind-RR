9708fbc0f0b93b8006cb308bd052b372baaad014
Arlen Neylon <arlen.m.neylon@gmail.com>
2022-06-23 12:24:56 -0600

    Add basic interaction.

--- src/components/TicTacToe/index.js
+++ src/components/TicTacToe/index.js
@@ -2,10 +2,23 @@ import React from 'react'
 import './TicTacToe.css'
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: null
+    }
+  }
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+        className="square"
+        onClick={() => {
+          // console.log(`clicked ${this.props.value}`)
+          this.setState({ value: 'X' })
+        }}
+        >
+        {/* {this.props.value} */}
+        {this.state.value}
       </button>
     );
   }
@@ -13,7 +26,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
