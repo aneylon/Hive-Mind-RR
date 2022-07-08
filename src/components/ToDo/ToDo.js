3dfc8041ebcf2f9f385e6fb765d1246245963db0
Arlen Neylon <arlen.m.neylon@gmail.com>
2022-07-08 17:17:05 -0600

    More todo setup.

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -2,16 +2,17 @@ import React from 'react'
 import AddToDo from './AddToDo'
 import ToDoList from './ToDoList'
 import ToDoFilters from './ToDoFilters'
+import './ToDoStyle.css'
 
 const ToDo = () => {
-  return (
-    <div className="todo-app">
-      <h1>Todo list</h1>
-      <AddToDo />
-      <ToDoList />
-      <ToDoFilters />
-    </div>
-  )
+	return (
+		<div className='todo-app'>
+			<h1>Todo list</h1>
+			<AddToDo />
+			<ToDoList />
+			<ToDoFilters />
+		</div>
+	)
 }
 
 export default ToDo
--- src/components/ToDo/ToDoList.js
+++ src/components/ToDo/ToDoList.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import ToDoItem from './ToDoItem'
 
-const ToDoList = () => {
-  return (
-    <div>
-      To Do List.
-    </div>
-  )
+import { getTodosByVisibilityFilter } from '../../redux/actions/selectors'
+
+const ToDoList = ({ todos }) => (
+	<ul className='todo-list'>
+		{todos && todos.length
+			? todos.map((todo, index) => {
+					return <ToDoItem key={`todo-${todo.id}`} todo={todo} />
+			  })
+			: 'Nothing To Do!'}
+	</ul>
+)
+
+const mapStateToProps = (state) => {
+	const { visibilityFilter } = state
+	const todos = getTodosByVisibilityFilter(state, visibilityFilter)
+	return { todos }
 }
 
-export default ToDoList
+export default connect(mapStateToProps)(ToDoList)
