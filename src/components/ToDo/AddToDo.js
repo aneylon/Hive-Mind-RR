1fd14175bbc34ddb3177eda6698f7f46cf0f68ce
Arlen Neylon <arlen.m.neylon@gmail.com>
2022-07-08 13:27:52 -0600

    Continue Todo setup.

--- src/components/ToDo/AddToDo.js
+++ src/components/ToDo/AddToDo.js
@@ -1,16 +1,36 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { addToDo } from '../../redux/actions/actions'
 
-const AddToDo = () => {
-  return (
-    <div>
-      <input
-        placeholder='Add a ToDo item.'/>
-      <button
-        onClick={console.log('adding todo!')}>
-        Add ToDo
-      </button>
-    </div>
-  )
+class AddToDo extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = { input: '' }
+	}
+
+	updateInput = (input) => {
+		this.setState({ input })
+	}
+
+	handleAddTodo = () => {
+		this.props.addToDo(this.state.input)
+		this.setState({ input: '' })
+	}
+
+	render() {
+		return (
+			<div>
+				<input
+					placeholder='Add a ToDo item.'
+					onChange={(e) => this.updateInput(e.target.value)}
+					value={this.state.input}
+				/>
+				<button className='add-todo' onClick={this.handleAddTodo}>
+					Add ToDo
+				</button>
+			</div>
+		)
+	}
 }
 
-export default AddToDo
+export default connect(null, { addToDo })(AddToDo)
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,10 @@ import { createStore } from 'redux'
 
 import rootReducer from './reducers'
 
-export default createStore(rootReducer)
+export default createStore(
+	rootReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 // move to actions and reducers
 
